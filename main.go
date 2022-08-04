e8fac0f432e62788fcb8d9bc9e3d7b6f773cc219
Yasin Bozat <37029016+yasinbozat@users.noreply.github.com>
2022-08-04 23:29:03 +0300

    finish

--- main.go
+++ main.go
@@ -8,20 +8,12 @@ import (
 	"gorm.io/gorm"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "1234"
-	dbname   = "db_user"
-)
-
 type User struct {
 	Id   int64  `gorm:"primary_key"`
 	Name string `gorm:"size:255"`
 }
 
-var dsn = "host=localhost user=postgres password=1234 dbname=db_user port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+var dsn = "host=localhost user=postgres password=1234 dbname=db_user port=5432 sslmode=disable TimeZone=Asia/Istanbul"
 
 func main() {
 
@@ -39,10 +31,4 @@ func main() {
 		fmt.Printf("ID:%d\nNickname:%s\n", user.Id, user.Name)
 		fmt.Printf("--------------------------------------------------------------\n")
 	}
-	/* 	app := fiber.New()
-	   	app.Get("/", func(c *fiber.Ctx) error {
-	   		return c.SendString("Nickname : nil")
-	   	})
-
-	   	app.Listen(":3000") */
 }
