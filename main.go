93172033e24caaac50981c073548c92b59d51f30
Yasin BOZAT <yasinbozatr@gmail.com>
2022-08-11 15:55:06 +0300

    deleted unnecessary commands

--- main.go
+++ main.go
@@ -8,11 +8,6 @@ import (
 func main() {
 
 	//database.DB.Debug().AutoMigrate(&models.User{},&models.Key{},&models.UserKey{}) // Auto Migration User Table
-	//AddUser(501, "Yasin", "Bozat", "admin@yasinbozat.com", "123456789", "+90 (543) 987 6543", "Turkey", "Sivas", "99:34:YB:23:BZ:58", db())
-	//fmt.Print(SelectUserName(501, db()))
 	models.Login("admin@yasinbozat.com", "123456789")
-	//DeleteUser(500)
-	//utils.RemainingTime()
-	//DB.Debug().AutoMigrate(&models.User{}) // Auto Migration User Table
-	//DB.Debug().AutoMigrate(&models.UserKey{})
+
 }
